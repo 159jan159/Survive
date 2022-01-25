1165d877c91884b17aaa61fc9c06209dd5e400e0
159jan159 <159honza159@gmail.com>
2022-01-25 16:18:03 +0100

    s

--- 7DTD/Assets/SuperTiled2Unity/Import/House.tsx
+++ 7DTD/Assets/SuperTiled2Unity/Import/House.tsx
@@ -3,17 +3,12 @@
  <image source="../../Art/Objects/house1_v1.png" width="256" height="256"/>
  <tile id="8">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="50" y="-54.25" width="114.25" height="131.25"/>
+   <object id="1" x="49.5259" y="-53.12" width="114.25" height="131.25"/>
   </objectgroup>
  </tile>
  <tile id="11">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-65.25" y="-54.5" width="116.5" height="90.5"/>
-  </objectgroup>
- </tile>
- <tile id="14">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="-11" y="1" width="9.25" height="10.75"/>
+   <object id="1" x="-65.6136" y="-53.1491" width="116.5" height="89.4771"/>
   </objectgroup>
  </tile>
 </tileset>
