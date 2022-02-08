70920f4e7e697ee37cf79133f717eede3b43af01
159jan159 <159honza159@gmail.com>
2022-02-08 19:08:54 +0100

    xd

--- 7DTD/Assets/Scripts/EnemyRelated/ZombieController.cs
+++ 7DTD/Assets/Scripts/EnemyRelated/ZombieController.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class ZombieController : MonoBehaviour
 {
-
-    private Rigidbody2D myRB;
     private Animator myAnim;
+    private float lastX;
+    private float lastY;
 
     
     void Start()
     {
-        
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        Debug.Log(lastX +"|"+ lastY);
+
+
         //animace
-        myAnim.SetFloat("MoveX", myRB.velocity.x);
-        myAnim.SetFloat("MoveY", myRB.velocity.y);
+        myAnim.SetFloat("MoveX", lastX-transform.position.x);
+        myAnim.SetFloat("MoveY", lastY-transform.position.y);
+
+           
+        lastX = transform.position.x;
+        lastY = transform.position.y;
 
         //if (Input.GetAxisRaw("Horizontal")==1 || Input.GetAxisRaw("Horizontal")==-1 || Input.GetAxisRaw("Vertical")==1 ||Input.GetAxisRaw("Vertical")==-1)
         //{
